58aebabad913608b9a46e5a77d177e6c80ec44dd
Veronica <sandrinveronica@gmail.com>
2024-06-03 16:35:55 +0200

    Completati esercizi su variabili e Costanti

---
+++ main.js
@@ -0,0 +1,16 @@
+var num1 = 5;
+var num2 = 10;
+
+console.log(`Il valore della variabile num1 e' ${num1}`);
+console.log(`Il valore della variabile num1 e' ${num2}`);
+
+var stringa = 'Ciao Mondo';
+console.log(`Il valore della variabile stringa è ${stringa}`);
+
+stringa = 'Arrivederci e Grazie';
+console.log(`Il valore della variabile stringa è ${stringa}`);
+
+const PIGRECO = 3.14159265359;
+console.log("il valore di PIGRECO e' " + PIGRECO);
+PIGRECO = 3.15639265359;
+console.log("il valore di PIGRECO e' " + PIGRECO);
